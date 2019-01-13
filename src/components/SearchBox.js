b79dc0af21fc2ead30b27ad26d79046626fafe9c
Ieremeiov Ciprian <ciprian.ieremeiov@gmail.com>
2019-01-14 01:51:13 +0200

    PWA improvements

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,6 +8,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className="pa2">
             <input className="pa3 ba b--green bg-lightest-blue"
+                   aria-label="Search Robots"
                    type="search"
                    placeholder="search robots"
                    onChange={searchChange}/>
